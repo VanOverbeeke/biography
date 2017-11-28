ea4cffe01f856a2763185e7d3e2aaabc12e04301
DESKTOP-OHBNL4H\lennert <l.vanoverbeeke@emendis.nl>
2017-11-28 16:55:28 +0100

    Initial population with Laravel and Angular.

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from "./biography/header/header.component";
+import { FooterComponent } from "./biography/footer/footer.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+      HeaderComponent,
+      FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
