86cd1d32a64c4352f744494a3ddc10c4abb6bc44
DESKTOP-OHBNL4H\lennert <l.vanoverbeeke@emendis.nl>
2018-01-05 14:48:38 +0100

    Fixed StorePicture

--- backend/app/Http/Requests/StorePicture.php
+++ backend/app/Http/Requests/StorePicture.php
@@ -23,10 +23,7 @@ class StorePicture extends FormRequest
     {
         $messages = [
             'string' => 'The :attribute must be of type string.',
-            'max' => 'The :attribute must have a max length of :max.',
             'required' => 'The :attribute is required.',
-            'url' => 'The :attribute must be of type url.',
-            'unique' => 'The :attribute must be unique in this table.',
             'numeric' => 'The :attribute must be of type numeric with maximum two decimal points.',
             'exists' => 'The :attribute must have an existing value.',
         ];
